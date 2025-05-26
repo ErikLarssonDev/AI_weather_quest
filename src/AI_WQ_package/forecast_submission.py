fed0df76e19f5b129ccf31f0460b63b24eb8bfe0
TomasLandelius <104756361+TomasLandelius@users.noreply.github.com>
2025-05-26 19:36:50 +0200

    Add support for creating empty fc xarray offline

--- src/AI_WQ_package/forecast_submission.py
+++ src/AI_WQ_package/forecast_submission.py
@@ -130,6 +130,107 @@ def AI_WQ_create_empty_dataarray(variable,fc_start_date,fc_period,teamname,model
 
     return da
 
+def AI_WQ_create_empty_dataarray_offline(variable,fc_start_date,fc_period,teamname,modelname):
+    ''' A function that creates an 'empty' dataarray and supports offline forecast evaluation for the AI Weather Quest. 
+    '''
+
+    # Check filename characteristics and output a string version of fc_period
+    # fc_period = check_fc_submission.check_filename_characteristics(variable,fc_start_date,fc_period,teamname,modelname)
+
+    # standard for all variables
+    standard_names_all_vars = {'units':'1','coordinates':'latitude longitude'}
+
+    # set standard names of variable
+    # need to add cell method - MEAN (tas, mslp) and SUM (pr).
+    if variable == 'mslp':
+        data_specs = {**{'standard_name':'Mean sea level pressure probability','cell_methods':'time: mean (interval: 6 hours)'},**standard_names_all_vars}
+    elif variable == 'tas':
+        data_specs = {**{'standard_name':'2 metre temperature probability','cell_methods':'time: mean (interval: 6 hours)'},**standard_names_all_vars}
+    elif variable == 'pr':
+        data_specs = {**{'standard_name':'Total precipitation probability','cell_methods':'time: sum (interval: 24 hours)'},**standard_names_all_vars}
+
+    # add shortName
+    if variable == 'pr':
+        shortName = 'tp'
+    elif variable == 'tas':
+        shortName = 't2m'
+    elif variable == 'mslp':
+        shortName = 'mslp'
+
+    # add a height dimension if tas
+    if variable == 'tas':
+        height = 2  # Assuming height is at near-surface level (2 m), modify if needed
+        height_attrs = {
+        'standard_name': 'height',
+        'units': 'm',
+        'positive': 'up',
+        'axis': 'Z'
+                        }
+    else:
+        height = None  # No height for other variables
+
+    fc_issue_date = fc_start_date[:4]+'-'+fc_start_date[4:6]+'-'+fc_start_date[6:]
+
+    # alongside defining the forecast issue date, define the forecasting period in days from forecasting issue date.
+    if fc_period == '1':
+        forecast_period_start = 18.0
+        if variable == 'mslp' or variable == 'tas':
+            forecast_period_end = 24.75
+        elif variable == 'pr':
+            forecast_period_end = 25.0
+    elif fc_period == '2':
+        forecast_period_start = 25.0
+        if variable == 'mslp' or variable == 'tas':
+            forecast_period_end = 31.75
+        elif variable == 'pr':
+            forecast_period_end = 32.0
+    forecast_period_bounds = [[forecast_period_start,forecast_period_end]]
+
+    # empty data
+    empty_data = np.empty((5,121,240))
+
+    # dimension attributes
+    lat_attrs = {'units':'degrees_north','long_name':'latitude','standard_name':'latitude','axis':'X'}
+    lon_attrs = {'units':'degrees_east','long_name':'longitude','standard_name':'longitude','axis':'Y'}
+    latitude = np.arange(90.0,-91.0,-1.5) # based on 1.5 deg grid
+    longitude = np.arange(0.0,360.0,1.5)
+
+    # create an appropriate identity names. Unique_ID = origin (characters from teamname [first four + last six - filled with 'z' if needed]). expver_ID (teamname plus '01', '02' etc... where number denotes model number (based on names already in look-up table).
+    # origin_id, expver_id = update_table_unique_identifies(teamname,modelname,password)
+
+    origin_id, expver_id = '0', '0'
+
+    # work out forecast issue time
+    fc_issue_time = np.datetime64(fc_issue_date+'T00:00:00')
+    # With the data, make a dataset array. Streamlining dataset creation so all submissions are the same.
+    da = xr.DataArray(data=empty_data,dims=['quintile','latitude','longitude'],
+            coords=dict(quintile=(['quintile'],np.arange(0.2,1.1,0.2)), # outputs [0.2,0.4,0.6,0.8,1.0]
+                        latitude=(['latitude'],latitude,lat_attrs),
+                        longitude=(['longitude'],longitude,lon_attrs),
+                        forecast_issue_date=fc_issue_time,
+                        forecast_period_start=fc_issue_time+np.timedelta64(int(forecast_period_start*24), 'h'),
+                        forecast_period_end=fc_issue_time+np.timedelta64(int(forecast_period_end*24), 'h'),
+                        height=height if height is not None else None
+                        ),
+            attrs=dict(**data_specs,description=variable+' prediction from '+teamname+' using '+modelname+' for forecasting period '+str(fc_period),
+                Conventions='CF-1.6',
+                forecast_period_bounds_units='days into forecast',
+                forecast_period_bounds=f"[{forecast_period_start},{forecast_period_end}]",
+                shortName=shortName,
+                originating_centre=origin_id,
+                expver=expver_id,
+                teamname=teamname,
+                modelname=modelname))
+    # add the time attrs
+    da.coords['forecast_issue_date'].attrs = {'standard_name': 'forecast_issue_time','long_name': 'forecast issue time','axis':'T'}
+    da.coords['forecast_period_start'].attrs = {'long_name': 'forecast period start','axis':'T'} 
+    da.coords['forecast_period_end'].attrs = {'long_name': 'forecast period end','axis':'T'}
+
+    if height is not None:
+        da.coords['height'].attrs = height_attrs
+
+    return da
+
 def AI_WQ_forecast_submission(data,variable,fc_start_date,fc_period,teamname,modelname,password):
     ''' This function will take a dataset in quintile, lat, long format, save as appropriate netCDF format,
     then copy to FTP site under correct forecast folder, i.e. 20241118. 
